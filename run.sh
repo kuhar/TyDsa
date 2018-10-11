02de82f879ee6511b9e90c67c21820b8c3992cd3
Jakub Kuderski <kubakuderski@gmail.com>
2018-10-11 13:23:15 -0400

    Workaround new souffle bugs

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 rm -f _current_test.dl
 cp $1 _current_test.dl
+mkdir -p ./out
 
 if [ $? -ne 0 ] ; then
 	echo "No such file: $1, aborting run"
 	exit -1
 fi
 
-souffle ty_dsa.dl -D- -v && souffle ty_dsa.dl -D./out -v >/dev/null 2>&1
+souffle ty_dsa.dl -D- --verbose && souffle ty_dsa.dl -D./out --verbose >/dev/null 2>&1
 
